f41e3e3658d929cb9745a33496d5db9ab60ab480
Damian Ysla <ysladamian@outlook.com>
2023-10-05 18:27:26 -0300

    change some styles for btn

--- src/components/Components.jsx
+++ src/components/Components.jsx
@@ -3,7 +3,7 @@ import "./components.css"
 export function Button({text}) {
 return (
   <button
-    className='w-full py-1 text-white text-lg font-semibold rounded-lg bg-color1 shadow-lgt'
+    className='w-full py-1 text-white text-lg font-semibold rounded-lg bg-color1 shadow-lg hover:bg-color1-light'
   >
     {text}
   </button>
